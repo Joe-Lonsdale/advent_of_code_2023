1e1d5ac86d6299f83aac7231b27cb0d9b11dad7f
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-12-06 12:45:10 +0000

    day6 wip

--- day6/main.py
+++ day6/main.py
@@ -1,6 +1,29 @@
-filename = "input.txt"
+times = [57726992]
+records = [291117211762026]
 
-file = open(filename, "r")
+ways_to_win = [0]
 
-for line in file:
-    
+def do_race(press_time, max_time):
+    dist = 0
+    speed = 0
+    for i in range(max_time):
+        if i < press_time:
+            speed += 1
+        else:
+            dist += speed
+    return dist
+
+
+for i in range(len(ways_to_win)):
+    for x in range(times[i]+1):
+        dist = do_race(x,times[i])
+        if dist > records[i]:
+            ways_to_win[i] += 1
+
+total = 1
+
+for x in ways_to_win:
+    total *= x
+print(total)
+
+
