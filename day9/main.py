9e8efeb6ab65158db8b63910463f728559e9536e
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-12-09 16:26:18 +0000

    improve skeleton

--- day9/main.py
+++ day9/main.py
@@ -3,4 +3,17 @@ filename = "input.txt"
 file = open(filename, "r")
 
 for line in file:
-    
+    break
+
+def part_one():
+    return
+
+def part_two():
+    return
+
+
+def main():
+    part_two()
+
+if __name__ == '__main__':
+    main()
