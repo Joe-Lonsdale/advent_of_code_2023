1f97a1e030f28df2db852dfb1c5b5089b963130b
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-12-09 17:10:38 +0000

    day9

--- day9/main.py
+++ day9/main.py
@@ -1,15 +1,50 @@
 filename = "input.txt"
+# filename = "test_input.txt"
 
 file = open(filename, "r")
 
-for line in file:
-    break
-
 def part_one():
+    sequences = []
+    for line in file:
+        sequences.append([int(x) for x in line.split(' ')])
+    total = 0
+    for seq in sequences:
+        seqs = break_down_seq(seq)
+        seqs.append(seq)
+        for s in seqs:
+            print(s)
+            total += s[-1]
+        print("==========================================")
+    print(total)
     return
 
 def part_two():
+    sequences = []
+    for line in file:
+        sequences.append([int(x) for x in line.split(' ')])
+    total = 0
+    for seq in sequences:
+        seqs = break_down_seq(seq)
+        seqs.append(seq)
+        subtotal = 0
+        for i in range(1, len(seqs)):
+            print(seqs[i])
+            subtotal = seqs[i][0] - subtotal
+            print(subtotal)
+        total += subtotal
+        print("==========================================")
+    print(total)
     return
+
+def break_down_seq(seq):
+    diffs = [seq[i] - seq[i-1] for i in range(1,len(seq))]
+    if(diffs.count(0)) == len(diffs):
+        return [diffs]
+    ret = break_down_seq(diffs)
+    ret.append(diffs)
+    return ret
+
+
 
 
 def main():
