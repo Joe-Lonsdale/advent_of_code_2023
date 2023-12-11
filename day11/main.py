f47e120d5a98d5a9367ea6b0797b07180f74d593
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-12-11 12:11:51 +0000

    day11 pt 1

--- day11/main.py
+++ day11/main.py
@@ -1,19 +1,59 @@
+import numpy as np
 filename = "input.txt"
+# filename = "test_input.txt"
 
-file = open(filename, "r")
+file = open(filename).readlines()
+file = [f.strip() for f in file]
 
-for line in file:
-    break
 
 def part_one():
+    grid = [[f for f in l] for l in file]
+    grid = expand(grid)
+    galaxies = find_galaxies(grid)
+    sum = 0
+    for i in galaxies:
+        for j in galaxies:
+            if i == j: continue
+            sum += find_distance(i,j)
+    print(sum/2)
     return
 
 def part_two():
     return
 
+def expand(grid):
+    grid = np.array(grid)
+
+    def process_and_expand(g):
+        expanded = []
+        for row in g:
+            expanded.append(row)
+            if '#' not in row:
+                for i in range(1000000):
+                    expanded.append(row)
+        return np.array(expanded)
+
+    expanded_grid = process_and_expand(grid)
+    expanded_grid = expanded_grid.T
+    expanded_grid = process_and_expand(expanded_grid)
+
+    return expanded_grid.T.tolist()
+
+def find_galaxies(grid):
+    coords = []
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '#':
+                coords.append((i,j))
+    return coords
+
+def find_distance(i,j):
+    return abs(i[0] - j[0]) + abs(i[1] - j[1])
+
+
 
 def main():
-    part_two()
+    part_one()
 
 if __name__ == '__main__':
     main()
