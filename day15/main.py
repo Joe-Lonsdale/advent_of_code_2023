ae11737502c93974204b8860ae9ae8d0efc2b0fd
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-12-21 18:55:39 +0000

    day15 pt 1

--- day15/main.py
+++ day15/main.py
@@ -3,9 +3,20 @@ filename = "input.txt"
 
 file = open(filename).readlines()
 file = [f.strip() for f in file]
+file = [f for f in file[0].split(',')]
 
+def HASH(val, input):
+    return ((val + ord(input)) * 17) % 256
 
 def part_one():
+    print(file)
+    tot = 0
+    for x in file:
+        val = 0
+        for y in x:
+            val = HASH(val, y)
+        tot += val
+    print(tot)
     return
 
 def part_two():
